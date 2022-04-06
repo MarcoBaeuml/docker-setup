4c31289aa99b86b3e9b13b445798b376b5fa2650
root <root@vmd85411.contaboserver.net>
2022-04-06 15:31:03 +0200

    add possibility to remove docker or reinstall if already exist

--- install.sh
+++ install.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
 install_docker() {
-	#download Docker using the official Docker download script
+	# download Docker using the official Docker download script
 	curl -sS https://get.docker.com | bash &> /dev/null &
-	loading_animation "Installing docker:" "\033[0;32mfinished\033[0m"
+	loading_animation "Installing Docker:" "\033[0;32mfinished\033[0m"
 	echo $(docker -v)
 }
 
-install_docker_compose()   {
+install_docker_compose() {
 	# get the latest version from the release page at GitHub
-	VERSION=$(curl -sS https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d') &&
-	# download Docker compose plugin
-	curl -sSL -o /usr/local/lib/docker/cli-plugins/docker-compose --create-dirs https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-$(uname -p) &&
-	chmod +x /usr/local/lib/docker/cli-plugins/docker-compose &
-	loading_animation "Installing docker compose:" "\033[0;32mfinished\033[0m"
+	( VERSION=$(curl -sS https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+	sleep 5
+	# download Docker compose V2 plugin
+	curl -sSL -o /usr/local/lib/docker/cli-plugins/docker-compose --create-dirs https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-$(uname -p)
+	chmod +x /usr/local/lib/docker/cli-plugins/docker-compose ) &
+	loading_animation "Installing Docker compose:" "\033[0;32mfinished\033[0m"
 	echo $(docker compose version)
+}
+
+remove_docker() {
+	( dpkg -l | grep -i docker
+	apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+	apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+	rm -rf /var/lib/docker /etc/docker
+	rm /etc/apparmor.d/docker
+	groupdel docker
+	rm -rf /var/run/docker.sock
+	rm -rf /usr/local/bin/docker-compose
+	rm -rf /usr/local/lib/docker/
+	rm -rf ~/.docker
+	rm -rf /usr/libexec/docker ) &> /dev/null &
+	loading_animation "Removing Docker + Docker compose:" "\033[0;32mfinished\033[0m"
 }
 
 loading_animation(){
@@ -32,5 +48,54 @@ loading_animation(){
 	echo -ne "\b$2\n"
 }
 
-install_docker
-install_docker_compose
+function dockerExistMenu() {
+	echo "It looks like Docker is already installed."
+	echo ""
+	echo "What do you want to do?"
+	echo "   1) Reinstall Docker"
+	echo "   2) Remove Docker"
+	echo "   3) Exit"
+	echo ""
+	until [[ $MENU_OPTION =~ ^[1-3]$ ]]; do
+		read -rp "Select an option [1-3]: " MENU_OPTION
+	done
+	case $MENU_OPTION in
+	1)
+		remove_docker
+		install_docker
+		install_docker_compose
+		;;
+	2)
+		remove_docker
+		;;
+	3)
+		echo "exiting..."
+		exit 0
+		;;
+	esac
+}
+
+function dockerDoesntExistMenu() {
+	echo "This script will install Docker + Docker compose V2 (Plugin)"
+	read -r -p "Do you want to continue? [y/N] " response
+	case "$response" in
+		[yY][eE][sS]|[yY]) 
+			install_docker
+			install_docker_compose
+			;;
+		*)
+			echo "exiting..."
+			exit 0
+			;;
+	esac
+}
+
+echo "Brilliant decision, Docker is awesome"
+echo "The git repository is available at: https://github.com/MarcoBaeuml/docker-setup"
+echo ""
+
+if [ -x "$(command -v docker)" ]; then
+	dockerExistMenu
+else
+	dockerDoesntExistMenu
+fi
